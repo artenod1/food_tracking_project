8569fbc1154d0887523110daf0759db88a2ad70b
Dominic Arteno <artenods@yahoo.com>
2020-12-31 13:51:57 -0500

    User cannot edit email address in profile page. New user must use a unique email address.

--- users/forms.py
+++ users/forms.py
@@ -6,12 +6,20 @@ from .models import Profile
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
 
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		if User.objects.filter(email=email).exists():
+			raise forms.ValidationError("A user with that email already exists")
+		return self.cleaned_data
+
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 
+
 class UserUpdateForm(forms.ModelForm):
 	email = forms.EmailField()
+	email.disabled = True
 
 	class Meta:
 		model = User
